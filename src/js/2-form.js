b44b5529813d5f5b671c15744f6b1acbdc1b9ede
DiWolf <kazakovdmitry92@gmail.com>
2026-01-15 02:02:52 +0200

    15/01/1/

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -29,6 +29,9 @@ if (saveData) {
 feedbackForm.addEventListener('submit', event => {
   event.preventDefault();
 
+  formData.email = formData.email.trim();
+  formData.message = formData.message.trim();
+
   if (formData.email === '' || formData.message === '') {
     alert('Fill please all fields');
   } else {
